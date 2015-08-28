f86c5adf9641c8563fd68786cfa0d1d92f86b68c
wam3483 <wmorrison@GFR-CVG-0012282.ga.afginc.com>
2015-08-28 18:12:09 -0400

    Adding wifispeaker service lib.

---
+++ WiFiSpeakerServiceLib/WiFiSpeakerServiceLib/IWiFiSpeakerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace WiFiSpeakerServiceLib
+{
+	[ServiceContract]
+	public interface IWiFiSpeakerService
+	{
+		[OperationContract]
+		ConfigResult SetConfiguration(ServiceConfigDataModel model);
+
+		ServiceConfigDataModel GetConfiguration();
+	}
+}
---
+++ WiFiSpeakerServiceLib/WiFiSpeakerServiceLib/ServiceConfigDataModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Net;
+namespace WiFiSpeakerServiceLib
+{
+	[DataContract]
+	public class ServiceConfigDataModel
+	{
+		public string ServerName { get; set; }
+		public string Version { get; set; }
+
+		public IEnumerable<Client> Clients { get; set; }
+		public IEnumerable<ConfiguredAudioSource> ConfiguredAudioSources { get; set; }
+		public IEnumerable<AudioSource> AudioSources { get; set; }
+	}
+
+	public enum ConfigResult
+	{
+		Failed,
+		Success,
+	}
+	
+	[DataContract]
+	public class Client
+	{
+		public string ClientName { get; set; }
+		public IPEndPoint Endpoint { get; set; }
+	}
+	
+	[DataContract]
+	public class ConfiguredAudioSource
+	{
+		public string ConfiguredName { get; set; }
+		public string HardwareName { get; set; }
+	}
+
+	[DataContract]
+	public class AudioSource
+	{
+		public string HardwareName { get; set; }
+		public string Status { get; set; }
+	}
+}
